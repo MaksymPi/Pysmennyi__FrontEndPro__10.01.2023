d5211012999f0e24c0342bb2fbff78824cccd23e
Maksym Pysmennyi <maksimviktorovichua@gmail.com>
2023-02-09 17:07:20 +0100

    home works 23,24

---
+++ homework__7/work_24/js/script.js
@@ -0,0 +1,17 @@
+// ДЗ 21. this chain
+
+let ladder = {
+  step: 0,
+  up: function () {
+    this.step++;
+    return this
+  },
+  down: function () {
+    this.step--;
+    return this
+  },
+  showStep: function () {
+    console.log((this.step));
+  }
+};
+ladder.up().up().down().showStep();
