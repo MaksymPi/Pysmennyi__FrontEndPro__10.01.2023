53a91eb17590463b0c20799a6740b9ec4a07e741
Maksym Pysmennyi <uzytkownik@MacBook-Pro.local>
2023-03-01 15:52:49 +0100

    home work 33

---
+++ homework__13/work_33/js/script.js
@@ -0,0 +1,40 @@
+// 1) Избавится от неуникальных элементов массива
+
+const numbersArray = [1,1,2,3,4,5,5,5,6];
+function filter(arr) {
+  return Array.from(new Set(numbersArray));
+}
+console.log(filter(numbersArray));
+
+// 2) Создать Map который будет по переданному объекту возвращать строку привествия
+
+const userBob = {name: 'Bob'};
+const userJane = { name: 'Jane'};
+
+function greetUser(user){
+  const map = new Map();
+  const mapJane = map.set(userJane, 'How your kids doing Jane?');
+  const mapBob = map.set(userBob, 'Hello Bob nice to see you');
+  return  user === userBob ? mapBob.get(user) : 
+          user === userJane ? mapJane.get(user) :
+          'Не коректне значення!';
+}
+console.log(greetUser(userJane));
+console.log(greetUser(userBob));
+
+// 3) Создать функцию по превращению Map в object или object в Map в зависимости от того что было передано в функцию.
+
+function transform(dataType) {
+if (dataType instanceof Map) {
+  return Object.fromEntries(dataType)
+}else if (typeof dataType === 'object'){
+  return Object.entries(dataType)
+}else
+  return 'Не коректне значення!'
+}
+const err = 'Не коректне значення!';
+console.log(transform(err));
+const obj = { name: 'Sid', gender: 'man' };
+console.log(transform(obj));
+const map = new Map([['name', 'Sid'], ['gender', 'man']]);
+console.log(transform(map));
