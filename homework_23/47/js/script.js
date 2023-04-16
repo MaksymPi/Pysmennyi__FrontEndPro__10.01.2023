6606d4fd1fe27dc58c7bc994d698ddc77476c394
Macbook <macbook@MacBook-Pro-Macbook.local>
2023-04-16 22:22:11 +0200

    homework_47_2

--- homework_23/47/js/script.js
+++ homework_23/47/js/script.js
@@ -1,6 +1,6 @@
 fetch("http://api.openweathermap.org/data/2.5/weather?q=Kiev&units=metric&APPID=5d066958a60d315387d9492393935c19")
-  .then(function (response) { return response.json() } )
-  .then(function (data) { 
+  .then(response => response.json())
+  .then(data => {
     document.querySelector('.city__name').textContent = data.name;
     document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '&deg';
     document.querySelector('.pressure').textContent = data.main.pressure;
@@ -9,6 +9,5 @@ fetch("http://api.openweathermap.org/data/2.5/weather?q=Kiev&units=metric&APPID=
     document.querySelector('.deg').textContent = data.wind.deg;
     document.querySelector('.icon').innerHTML = `<img src="http://openweathermap.org/img/w/${data.weather[0]['icon']}.png">`;
   })
-
   .catch(error => console.log('error', error));
 
